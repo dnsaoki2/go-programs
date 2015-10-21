ceb3c5873e9da83253a0d38d38caa93a8c6f4dbb
Denis Aoki <dnsaoki@gmail.com>
2015-10-21 15:56:48 -0200

    a

--- dd/untitled.go
+++ dd/untitled.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	"gopkg.in/mgo.v2"
-	"gopkg.in/mgo.v2/bson"
-	"log"
+	// "gopkg.in/mgo.v2"
+	// "gopkg.in/mgo.v2/bson"
+	// "log"
 )
 
 type ins struct {
@@ -13,20 +13,21 @@ type ins struct {
 }
 
 func main() {
-	session, err := mgo.Dial("127.0.0.1:27017") //os.Getenv("MONGODB_URI"))
-	if err != nil {
-		panic(err)
-	}
-	defer session.Close()
-	c := session.DB("apidb").C("news")
-	str := ins{"1", "t"}
-	err = c.Insert(str)
-	if err != nil {
-		log.Fatal(err)
-	}
-	tam, err := c.Find(bson.M{"teste": "t"}).Count()
-	if err != nil {
-		fmt.Println("err", err)
-	}
+	// session, err := mgo.Dial("127.0.0.1:27017") //os.Getenv("MONGODB_URI"))
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer session.Close()
+	// c := session.DB("apidb").C("news")
+	// str := ins{"1", "t"}
+	// err = c.Insert(str)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// tam, err := c.Find(bson.M{"teste": "t"}).Count()
+	// if err != nil {
+	// 	fmt.Println("err", err)
+	// }
+	tam := ins{"string", "t"}
 	fmt.Println(tam)
 }
